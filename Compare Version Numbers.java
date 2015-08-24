ecde7030a4f5d52000cd3d490d87fe79dcd9f6ab
Angel Qian <angelqian2@gmail.com>
2015-08-24 14:20:57 -0700

    Update Compare Version Numbers.java

--- Compare Version Numbers.java
+++ Compare Version Numbers.java
@@ -14,3 +14,18 @@ public class Solution {
 	}
 
 }
+==================================================================
+public class Solution {
+    public int compareVersion(String version1, String version2) {
+        //正则表达式split("\\.")
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        for(int i=0, j=0; i<v1.length || j<v2.length; i++, j++){
+            int n1=i<v1.length? Integer.parseInt(v1[i]):0;
+            int n2=j<v2.length? Integer.parseInt(v2[j]):0;
+            if(n1 != n2)
+                return n1>n2? 1:-1;
+        }
+        return 0;
+    }
+}
