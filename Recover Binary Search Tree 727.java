d54551ef69706132702efa35ae21fda7a443fdfc
Angel Qian <angelqian2@gmail.com>
2015-11-20 15:54:09 -0800

    Update Recover Binary Search Tree 727.java

--- Recover Binary Search Tree 727.java
+++ Recover Binary Search Tree 727.java
@@ -33,3 +33,39 @@ public class Solution {
         traverse(cur.right);
     }
 }
+===================================================stack,
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private TreeNode nd1=null, nd2=null, pre = new TreeNode(Integer.MIN_VALUE);
+    public void recoverTree(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        while(root!=null || !stack.isEmpty()){
+            if(root!=null){
+                stack.push(root);
+                root = root.left;
+            }else if(!stack.isEmpty()){
+                TreeNode cur = stack.pop();
+                if(pre.val>=cur.val){
+                    if(nd1==null)
+                        nd1=pre;
+                    if(nd1!=null)
+                        nd2=cur;
+                }
+                root=cur.right;
+                pre = cur;
+            }
+        }
+        
+        int val = nd1.val;
+        nd1.val = nd2.val;
+        nd2.val = val;
+    }
+}
