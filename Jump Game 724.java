d891a363732caef24d6ec6e7820fef441df71e8a
Angel Qian <angelqian2@gmail.com>
2015-11-21 13:05:34 -0800

    Update Jump Game 724.java

--- Jump Game 724.java
+++ Jump Game 724.java
@@ -1,11 +1,27 @@
-//DP
+//DP1: how much energy left at f[i]
 public class Solution {
     public boolean canJump(int[] nums) {
         int[] f = new int[nums.length];
-        f[0]=0;
-        for(int i=1; i<f.length; i++){
-            f[i] = Math.max(f[i-1],nums[i-1])-1;
+        f[0]=nums[0];
+        
+        for(int i=1; i<nums.length; i++){
+            f[i] = Math.max(f[i-1]-1,nums[i-1]-1);
             if(f[i]<0)
+                return false;
+        }
+        return true;
+    }
+}
+
+//DP2: f[i]: how far I can reach at f[i]
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int[] f=new int[nums.length+1];
+        f[0]=0;
+        
+        for(int i=0; i<nums.length; i++){
+            f[i+1]=Math.max(i+nums[i], f[i]);
+            if(f[i]<i)
                 return false;
         }
         return true;
