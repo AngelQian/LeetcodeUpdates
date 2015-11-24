dd5743f331dc4b57e123f58422318f18ebe5361e
Angel Qian <angelqian2@gmail.com>
2015-11-24 11:07:06 -0800

    Update Reverse Bits .java

--- Reverse Bits .java
+++ Reverse Bits .java
@@ -24,3 +24,15 @@ public class Solution {
         return result;
     }
 }
+=========================================================================================
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result=0;
+        for(int i=0; i<32; i++){
+            result=result*2+n%2;
+            n>>>=1;
+        }
+        return result;
+    }
+}
