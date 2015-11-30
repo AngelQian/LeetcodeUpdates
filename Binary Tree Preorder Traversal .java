5e827c188ebfeb2f4ed174223c653f7ced57da8b
Angel Qian <angelqian2@gmail.com>
2015-11-30 15:49:12 -0800

    Update Binary Tree Preorder Traversal .java

--- Binary Tree Preorder Traversal .java
+++ Binary Tree Preorder Traversal .java
@@ -8,18 +8,38 @@
  * }
  */
 public class Solution {
-	//output left child and log right child
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> li = new ArrayList<>();
-        Stack<TreeNode> sk_right = new Stack<>();
-        while(root!=null){
-            li.add(root.val);
-            if(root.right!=null)
-                sk_right.add(root.right);
-            root = root.left;
-            if(root==null && !sk_right.isEmpty())
-                root = sk_right.pop();
+        // 走左边，存右边， 
+        // 边走边存
+        //中，左，右
+        List<Integer> result = new ArrayList<>();
+        if(root==null)
+            return result;
+            
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode tmp = stack.pop();
+            result.add(tmp.val);
+            if(tmp.right!=null)
+                stack.push(tmp.right);
+            if(tmp.left!=null)
+                stack.push(tmp.left);
         }
-        return li;
+        return result;
+    }
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while(root!=null){
+            result.add(root.val);
+            if(root.right!=null)
+                stack.push(root.right);
+            root=root.left;
+            if(root==null&&!stack.isEmpty())
+                root=stack.pop();
+        }
+        return result;
     }
 }
