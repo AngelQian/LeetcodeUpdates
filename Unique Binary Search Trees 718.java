7a887b36a4031902c7e608cfe8dbe0cefbc8351f
Angel Qian <angelqian2@gmail.com>
2015-11-30 16:52:28 -0800

    Update Unique Binary Search Trees 718.java

--- Unique Binary Search Trees 718.java
+++ Unique Binary Search Trees 718.java
@@ -10,3 +10,22 @@ public class Solution {
     }
    
 }
+-------------------------OPT
+public class Solution {
+    public int numTrees(int n) {
+        /*f[0]=1;
+        f[1]=f[0]*f[0]=1;
+        f[2]=f[0]*f[1]+f[0]*f[0]=1+1=2;
+        f[3]=f[0]*f[2]+f[1]*f[1]+f[2]*f[0]=1*2+1*1+2*1=5;
+        f[4]=f[0]*f[3]+f[1]*f[2]+f[2]*f[1]+f[3]*f[0]=...;*/
+        
+        int[] f = new int[n+1];
+        f[0]=1; 
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                f[i]+=f[j]*f[i-j-1];
+            }
+        }
+        return f[n];
+    }
+}
