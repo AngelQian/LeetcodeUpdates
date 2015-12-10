75ac4b4815e027fe79488a3ee6745ba8f96bd0cf
Angel Qian <angelqian2@gmail.com>
2015-12-09 17:11:25 -0800

    Update Kth Largest Element in an Array.java

--- Kth Largest Element in an Array.java
+++ Kth Largest Element in an Array.java
@@ -1,26 +1,38 @@
+Kth Largest Element in an Array
+
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int left=0, right=nums.length-1;
+        //Quick sort theory, find partition
+        int left=0, right=nums.length-1, val;
         while(left<=right){
-            int pivot = nums[left];
-            int tmp=left+1, cur=tmp;
-            while(cur<=right){
-                if(nums[cur]>pivot)
-                {
-                    swap(nums, cur, tmp++);
-                }
-                cur++;
-            }
-            swap(nums, left, tmp-1);
+            val=nums[left];
+            int pos = getIndex(val, nums, left+1, right);
+                
+            swap(nums, pos-1, left); 
             
-            if(tmp==k)
-                return pivot;
-            else if(tmp<k)
-                left=tmp;
+            if(pos==k)
+                return nums[pos-1];
+            
+            if(pos>k)
+                right=pos-2;
             else
-                right=tmp-2;
+                left=pos;   
         }
-        return 0;
+        return -1;
+    }
+    
+    private int getIndex(int val, int[] nums, int left, int right){
+        while(left<=right){
+            if(nums[left]<val)
+                swap(nums, left, right--);
+            else if(nums[right]>val)
+                swap(nums, left++, right);
+            else{
+                left++;
+                right--;
+            }    
+        }
+        return left;
     }
     
     private void swap(int[]nums, int left, int right){
