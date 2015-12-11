9b4995451a4af6e3d1edb3aad94f55771351a239
Angel Qian <angelqian2@gmail.com>
2015-12-10 16:10:51 -0800

    Update Pow(x, n) 724.java

--- Pow(x, n) 724.java
+++ Pow(x, n) 724.java
@@ -1,3 +1,30 @@
+//...12.10
+Pow(x, n)
+public class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            long exp=-n;
+            return 1/helper(x,exp);
+        }else
+            return helper(x,n);
+    }
+    
+    private double helper(double x, long n){
+        if(n==0)
+            return 1;
+        if(n==1)
+            return x;
+        
+        if(n%2==1)  
+            return helper(x, n-1)*x;
+        else{
+            double tmp = helper(x, n/2);    
+            return tmp*tmp; 
+        }
+    }
+}
+===========================================================
+
 public class Solution {
     public double myPow(double x, int n) {
         // X^n = X^(n/2)*X^(n/2)*X^(n%/2)
