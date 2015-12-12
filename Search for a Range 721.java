932fb215c101799b409842e0928a51b38f8e5c22
Angel Qian <angelqian2@gmail.com>
2015-12-11 21:42:56 -0800

    Update Search for a Range 721.java

--- Search for a Range 721.java
+++ Search for a Range 721.java
@@ -1,4 +1,30 @@
 public class Solution {
+    private Integer l=Integer.MAX_VALUE, r=Integer.MIN_VALUE;
+	    
+	    public int[] searchRange(int[] nums, int target) {
+	        helper(nums, target, 0, nums.length-1);
+	        return l==Integer.MAX_VALUE?new int[]{-1,-1}:new int[]{l, r};
+	    }
+	    
+	    private void helper(int[] nums, int target, Integer left, Integer right){
+	        if(left>right)
+	            return;
+	        int mid=(left+right)/2;
+	        if(nums[mid]==target){
+	            l=Math.min(l,mid);
+	            r=Math.max(r,mid);
+	            helper(nums, target, left, mid-1);
+	            helper(nums, target, mid+1, right);
+	        }else if(nums[mid]>target)
+	            helper(nums, target, left, mid-1);
+	        else
+	            helper(nums, target, mid+1, right);
+	    }
+}
+
+=======================================================
+
+public class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] result = {-1,-1};
         
