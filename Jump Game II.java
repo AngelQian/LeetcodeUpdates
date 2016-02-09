2db36996241d9654c37badd9e6098f6d3be6aa86
Angel Qian <angelqian2@gmail.com>
2016-02-09 15:16:20 -0800

    Update Jump Game II.java

--- Jump Game II.java
+++ Jump Game II.java
@@ -24,4 +24,27 @@ public class Solution {
         }
         return step;
     }
+    public int jump(int[] nums) {
+        int step=0;
+        int left=0,right=0;
+        if(nums.length<2)
+            return step;
+        
+        while(left<=right){
+            step ++;
+            int pre_right = right;
+            int start=left;
+            while(start<=pre_right){
+                int next_right = start+nums[start];
+                if(next_right>=nums.length-1)
+                    return step;
+                    
+                right = Math.max(next_right, right);    
+                start++;
+            }
+            left = pre_right+1;
+        }    
+        
+        return 0;
+    }
 }
