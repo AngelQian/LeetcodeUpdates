c9a8f1b316c566940bec8a9201b950a4250c3c36
Angel Qian <angelqian2@gmail.com>
2016-02-18 22:46:21 -0800

    Update Edit Distance 729.java

--- Edit Distance 729.java
+++ Edit Distance 729.java
@@ -30,3 +30,24 @@ public class Solution {
         return f[word1.length()][word2.length()];
     }
 }
+
+=================
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] f = new int[word1.length()+1][word2.length()+1];
+        for(int i=0; i<=word1.length(); i++){
+            for(int j=0; j<=word2.length(); j++){
+                if(i==0||j==0){
+                    f[i][j]=Math.max(i,j);
+                }else{
+                    if(word1.charAt(i-1)==word2.charAt(j-1))
+                        f[i][j]=f[i-1][j-1];
+                    else
+                        f[i][j] = Math.min(f[i-1][j-1], Math.min(f[i-1][j], f[i][j-1]))+1;
+                }
+            }
+        }
+        return f[word1.length()][word2.length()];
+    }
+}
+
