b359ce3672ea8be34412a041d3ff747972817f58
Angel Qian <angelqian2@gmail.com>
2016-02-23 18:02:09 -0800

    Update Word Search 720.java

--- Word Search 720.java
+++ Word Search 720.java
@@ -1,38 +1,38 @@
 public class Solution {
+    int row, col;
     public boolean exist(char[][] board, String word) {
-        int row = board.length;
-        int col = board[0].length;
-        boolean[][] f = new boolean[row][col];
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(helperDFS(board, word, f, i, j, 0))
-                    return true;
-            }
-        }
+        row = board.length; col = board[0].length;
+        boolean[][] visited = new boolean[row][col];
         
-        return false;
+        for(int r=0; r<row; r++){
+            for(int c=0; c<col; c++){
+                if(hasCharacter(0, word, r, c, board, visited)){
+                    return true;
+                }
+            } 
+        }   
+        return false;   
     }
     
-    private boolean helperDFS(char[][] board, String word, boolean[][] f, int x, int y, int index){
-        if(index == word.length())
+    private boolean hasCharacter(int index, String word, int r, int c, char[][] board, boolean[][] visited){
+        if(index>=word.length())
             return true;
             
-        if(x<=0 || y<=0 || x>board.length || y>board[0].length)
+        if(r>=row || c>=col || r<0 || c<0)
             return false;
-           
-        if(f[x][y])
+        if(visited[r][c] == true)
             return false;
-            
-        if(board[x][y] != word.charAt(index)) 
+        if(word.charAt(index)!=board[r][c])
             return false;
         
-        f[x][y]=true;
-        boolean result = helperDFS(board, word, f, x, y+1, index+1)
-                        ||helperDFS(board, word, f, x, y-1, index+1)
-                        ||helperDFS(board, word, f, x+1, y, index+1)
-                        ||helperDFS(board, word, f, x-1, y, index+1);
-        f[x][y]=false;
+        visited[r][c] = true;
+        boolean val = hasCharacter(index+1, word, r+1, c, board, visited) ||
+            hasCharacter(index+1, word, r-1, c, board, visited)||
+            hasCharacter(index+1, word, r, c+1, board, visited)||
+            hasCharacter(index+1, word, r, c-1, board, visited);
+        if(val==false)
+            visited[r][c] = false;
         
-        return result;    
+        return val;    
     }
 }
