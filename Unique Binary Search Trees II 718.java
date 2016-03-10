dcd5c7ae866644f59f3062e33319c483448bff67
Angel Qian <angelqian2@gmail.com>
2016-03-09 20:31:18 -0800

    Update Unique Binary Search Trees II 718.java

--- Unique Binary Search Trees II 718.java
+++ Unique Binary Search Trees II 718.java
@@ -10,30 +10,28 @@
 public class Solution {
     public List<TreeNode> generateTrees(int n) {
         if(n==0)
-            return generateTreesHelper(1,0);
-        return generateTreesHelper(1,n);
+            return new ArrayList<TreeNode>();
+        return helper(1, n);    
     }
-    
-    private List<TreeNode> generateTreesHelper(int start, int end){
+    private List<TreeNode> helper(int start, int end){
         List<TreeNode> subTree = new ArrayList<>();
-        if(start > end){
+        if(start>end){
             subTree.add(null);
             return subTree;
-        }
-        
-        for(int k=start; k<=end; k++){
-            List<TreeNode> leftTree = generateTreesHelper(start, k-1);
-            List<TreeNode> rightTree = generateTreesHelper(k+1, end);
-            for(TreeNode left : leftTree){
-                for(TreeNode right: rightTree){
-                    TreeNode node = new TreeNode(k);
-                    node.left = left;
-                    node.right = right;
-                    subTree.add(node);
+        }    
+            
+        for(int nd=start; nd<=end; nd++){
+            List<TreeNode> leftTree = helper(start, nd-1);
+            List<TreeNode> rightTree = helper(nd+1, end);
+            for(TreeNode left:leftTree){
+                for(TreeNode right:rightTree){
+                    TreeNode cur = new TreeNode(nd);
+                    cur.left = left;
+                    cur.right = right;
+                    subTree.add(cur);
                 }
             }
         }
-        
         return subTree;
     }
 }
