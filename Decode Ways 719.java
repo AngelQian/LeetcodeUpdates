f6b474c0e3bc5d5a8ae51b72e73e733c8201a5b6
Angel Qian <angelqian2@gmail.com>
2016-03-09 23:25:04 -0800

    Update Decode Ways 719.java

--- Decode Ways 719.java
+++ Decode Ways 719.java
@@ -3,19 +3,18 @@ public class Solution {
         int[] f = new int[s.length()+1];
         if(s.length()<1)
             return 0;
-        
-        f[0]=1; 
+            
+        f[0]=1;
         for(int i=1; i<f.length; i++){
             if(s.charAt(i-1)!='0')
-                f[i]+=f[i-1];
-            
+                f[i] = f[i-1];
+                
             if(i>1){
-                int n = Integer.parseInt(s.substring(i-2,i));
-                if(n<=26 && n>=10)
-                    f[i]+=f[i-2];
+                Integer num = Integer.parseInt(s.substring(i-2, i));
+                if(num>=10 && num<=26)
+                    f[i] += f[i-2];
             }    
         }
-        
-        return f[f.length-1];
+        return f[s.length()];
     }
 }
