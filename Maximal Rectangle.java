215e780a82deacdc74c08219d946245745568995
Angel Qian <angelqian2@gmail.com>
2016-03-11 14:32:58 -0800

    Update Maximal Rectangle.java

--- Maximal Rectangle.java
+++ Maximal Rectangle.java
@@ -36,7 +36,7 @@ public class Solution {
     }
     
     private int getArea(int[] height, int val){
-        //all number equal or less than val
+        //all number equal or greater than val
         int width=0, tmp=0;
         for(int i=0; i<height.length; i++){
             if(height[i]>=val)
@@ -49,3 +49,47 @@ public class Solution {
     }
 }
 //https://leetcode.com/discuss/52670/solution-based-maximum-rectangle-histogram-with-explanation
+------
+//比较好的解法
+public class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int row = matrix.length;
+        if(row<1)
+            return 0;
+        int col=matrix[0].length;
+        int[] heights = new int[col];
+        int area = 0;
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(matrix[i][j]=='1')
+                    heights[j] += 1;
+                else
+                    heights[j] = 0;
+            }
+            area = Math.max(area, largestRectangleArea(heights));
+        }
+        return area;
+    }
+    
+    public int largestRectangleArea(int[] heights) {
+        //append 0 at the end;
+        int[] copyH = new int[heights.length+1];
+        for(int i=0; i<heights.length; i++)
+            copyH[i]=heights[i];
+        copyH[heights.length]=0;
+        
+        //accumulate increasing number and calculate max area once decreasing
+        int cur=0, area=0;
+        Stack<Integer> stW = new Stack<>();
+        while(cur<copyH.length){
+            if(stW.isEmpty() || copyH[cur]>=copyH[stW.peek()])
+                stW.push(cur++);
+            else{
+                int h = copyH[stW.pop()];
+                int w = stW.isEmpty() ? cur : cur-stW.peek()-1;
+                area = Math.max(area, w*h);
+            }    
+        }
+        return area;    
+    }
+}
